66898805d5dbb28e47b6448953dcfd9e318f4c6f
Rodrigo G Ramirez M <jadwer@msn.com>
2021-09-27 11:35:00 -0500

    first npm run files generation

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,7 +2,7 @@ let mix = require('laravel-mix');
 
 mix
     .js('assets/js/app.js', 'assets/js/scripts.js')
-    .sass('assets/css/jwr-theme-style.scss', 'assets/css/style.css')
+    .sass('assets/css/jwr-alea-style.scss', 'assets/css/style.css')
     .options({
         processCssUrls: false, // Process/optimize relative stylesheet url()'s. Set to false, if you don't want them touched.
         postCss: [require('tailwindcss')], // Post-CSS options: https://github.com/postcss/postcss/blob/master/docs/plugins.md
